b4a8e9dd7050e8b7744f3e969a3c904e6e33d875
D-damasus <113719811+D-damasus@users.noreply.github.com>
2022-11-11 23:54:49 +0100

    Update 0-malloc_checked.c

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -2,19 +2,19 @@
 #include "main.h"
 
 /**
-* malloc_checked - allocates memory using malloc
-* @b: size of memory to be allocated
-* Return: returns pointer to allocated memory on success
-* 98 if insufficient memory was available (FAILURE)
-*/
-
+ * *malloc_checked - allocates memory using malloc
+ * @b: number of bytes to allocate
+ *
+ * Return: a pointer to the allocated memory
+ */
 void *malloc_checked(unsigned int b)
 {
-	void *p;
+	void *ptr;
 
-	p = malloc(b);
-	if (p == NULL)
+	ptr = malloc(b);
+
+	if (ptr == NULL)
 		exit(98);
 
-	return (p);
+	return (ptr);
 }
